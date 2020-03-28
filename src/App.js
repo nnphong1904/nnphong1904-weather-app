cf5f36321e85f751c7719828f56bb0dc6357d7fc
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2020-03-28 23:23:24 +0700

    stuck in changint font family

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import '../src/assets/css/App.css';
 import CurrentWeather from './components/CurrentWeather';
+import WebFont from 'webfontloader';
 const API_KEY = 'AYht5xq9k0cECs3SFaOkgbQzZASinW8a';
 function App() {
+  WebFont.load({
+    google: {
+      families: ['Butcherman', 'cursive'],
+    },
+  });
   const content = (
     <div className="App">
       <main>
         <div className="search-box">
           {' '}
           <input className="search-bar" type="text" placeholder="Search..." />
-          <CurrentWeather apiKey={API_KEY} />
         </div>
+        <CurrentWeather apiKey={API_KEY} />
       </main>
     </div>
   );
