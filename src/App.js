3adf78d964b2856da36b07399982c7d97d04d4fc
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2020-03-29 18:58:54 +0700

    restruct the app by moving fetch function to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../src/assets/css/App.css';
 import CurrentWeather from './components/CurrentWeather';
+import axios from 'axios';
 const API_KEY = 'AYht5xq9k0cECs3SFaOkgbQzZASinW8a';
 function App() {
+  // state declaration
   const [searchValue, setSearchValue] = useState('');
   const [searchText, setSearchText] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState('');
+  const [currentWeatherData, setCurrentWeatherData] = useState({});
+  // extra function
+  const fetch = async (url) => {
+    // fetch funtion for fetching data and return promise
+    const response = await axios.get(url);
+    return response;
+  };
+  const dayBuilder = () => {
+    // day builder function to get the time with the format Sunday 13 March 2020
+    const date = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    const month = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const day = new Date();
+    return `${date[day.getDay()]} ${day.getDate()} ${
+      month[day.getMonth()]
+    } ${day.getFullYear()} `;
+  };
+  // fetching data
+  useEffect(() => {
+    if (searchText === '') {
+      navigator.geolocation.getCurrentPosition((position) => {
+        fetch(
+          `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${position.coords.latitude}%2C${position.coords.longitude}`
+        ).then((res) => {
+          const position = res.data.LocalizedName;
+          setLocation(res.data.LocalizedName);
+          fetch(
+            `https://dataservice.accuweather.com/currentconditions/v1/${res.data.Key}?apikey=${API_KEY} `
+          ).then((res) => {
+            const date = dayBuilder();
+            setCurrentWeatherData({ ...res.data[0], date, position });
+            setIsLoading(false);
+          });
+        });
+      });
+    } else {
+    }
+  }, [searchText]);
+  // handle event of search box
   const handleOnChange = (event) => {
     setSearchValue(event.target.value);
   };
@@ -13,6 +76,7 @@ function App() {
     event.preventDefault();
     setSearchValue('');
   };
+  // rendering content
   const content = (
     <div className="App">
       <main>
@@ -28,7 +92,7 @@ function App() {
             />
           </form>
         </div>
-        <CurrentWeather searchText={searchText} apiKey={API_KEY} />
+        <CurrentWeather data={currentWeatherData} />
       </main>
     </div>
   );
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,86 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import '../assets/css/CurrentWeather.css';
 function CurrentWeather(props) {
-  const { searchText, apiKey } = props;
-  const fetch = async (url) => {
-    const response = await axios.get(url);
-    return response;
-  };
-  const getX = (x) => x;
-  const dayBuilder = () => {
-    const date = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ];
-    const month = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    const day = new Date();
-    return `${date[day.getDay()]} ${day.getDate()} ${
-      month[day.getMonth()]
-    } ${day.getFullYear()} `;
-  };
-  const [isLoading, setIsLoading] = useState(true);
-  const [weatherData, setWeatherData] = useState();
-  const [location, setLocation] = useState('');
-  useEffect(() => {
-    // fetch data of current position when open app first time
-    if (searchText === '') {
-      navigator.geolocation.getCurrentPosition((position) => {
-        fetch(
-          `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${position.coords.latitude}%2C${position.coords.longitude}`
-        ).then((res) => {
-          setLocation(res.data.LocalizedName);
-          fetch(
-            `https://dataservice.accuweather.com/currentconditions/v1/${res.data.Key}?apikey=${apiKey} `
-          ).then((res) => {
-            setWeatherData(res.data[0]);
-            setIsLoading(false);
-          });
-        });
-      });
-    }
-    // fetch(
-    //   `https://dataservice.accuweather.com/currentconditions/v1/328328?apikey=${apiKey} `
-    // ).then((res) => {
-    //   setWeatherData(res.data[0]);
-    //   setIsLoading(false);
-    // });
-  }, []);
+  console.log(data);
   const content = (
-    <div className="current-weather-container">
-      {!isLoading && (
+    <>
+      {Object.entries(data).length !== 0 && (
         <>
           <div className="location-box">
-            <div className="location">{location}</div>
-            <div className="date">{dayBuilder()}</div>
+            <div className="location">{data.location}</div>
+            <div className="date">{data.date}</div>
           </div>
           <div className="weather-box">
             <div className="temp">
-              {`${parseInt(weatherData.Temperature.Metric.Value)}°C`}
+              {`${parseInt(data.Temperature.Metric.Value)}°C`}
             </div>
-            <div className="weather">{weatherData.WeatherText}</div>
+            <div className="weather">{data.WeatherText}</div>
           </div>
         </>
       )}
-    </div>
+    </>
   );
   return content;
 }
