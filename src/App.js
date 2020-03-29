370a8f04db76e4fab04480edf3613f49e31a21d1
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2020-03-29 19:01:39 +0700

    remove uneccessery state

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
   // state declaration
   const [searchValue, setSearchValue] = useState('');
   const [searchText, setSearchText] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [location, setLocation] = useState('');
   const [currentWeatherData, setCurrentWeatherData] = useState({});
   // extra function
   const fetch = async (url) => {
@@ -54,13 +52,11 @@ function App() {
           `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${position.coords.latitude}%2C${position.coords.longitude}`
         ).then((res) => {
           const position = res.data.LocalizedName;
-          setLocation(res.data.LocalizedName);
           fetch(
             `https://dataservice.accuweather.com/currentconditions/v1/${res.data.Key}?apikey=${API_KEY} `
           ).then((res) => {
             const date = dayBuilder();
             setCurrentWeatherData({ ...res.data[0], date, position });
-            setIsLoading(false);
           });
         });
       });
